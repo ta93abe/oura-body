f60101e09a0311403b93172d85194b16244b5e63
ta93abe <ta93abe@ta93abe.com>
2022-03-28 22:20:44 +0900

    styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,19 +13,19 @@ export interface ServerSideIndexProps {
 
 const Home = ({ props }: ServerSideIndexProps) => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen px-8">
       <Head>
         <title>our Oura</title>
         <meta name="description" content="fetch data from Oura api" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="flex-grow px-0 flex-1 flex flex-col justify-center items-center">
         <div>Heart Rate</div>
-        <div>{ props.reduce((sum, data) => sum + data.bpm, 0) / props.length }</div>
+        <div className="text-4xl">{ (props.reduce((sum, data) => sum + data.bpm, 0) / props.length).toFixed(1) }</div>
       </main>
 
-      <footer>
+      <footer className="flex py-8 px-0 border-t-2 border-solid border-l-violet-300 justify-center items-center">
         <a
           href="https://ta93a.be"
           target="_blank"
