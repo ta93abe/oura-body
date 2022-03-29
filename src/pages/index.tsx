47689f17ac6f5bbb29b8e3b146c7a5a76ba73ebd
ta93abe <ta93abe@ta93abe.com>
2022-03-29 12:22:59 +0900

    change background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,16 +13,16 @@ export interface ServerSideIndexProps {
 
 const Home = ({ props }: ServerSideIndexProps) => {
   return (
-    <div className="flex flex-col min-h-screen px-8">
+    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">    
       <Head>
         <title>our Oura</title>
         <meta name="description" content="fetch data from Oura api" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <div className="flex flex-col min-h-screen px-8">
       <main className="flex-grow px-0 flex-1 flex flex-col justify-center items-center">
         <div>Heart Rate</div>
-        <div className="text-4xl">{ (props.reduce((sum, data) => sum + data.bpm, 0) / props.length).toFixed(1) }</div>
+        <div className="text-8xl font-bold">{ (props.reduce((sum, data) => sum + data.bpm, 0) / props.length).toFixed(1) }</div>
       </main>
 
       <footer className="flex py-8 px-0 border-t-2 border-solid border-l-violet-300 justify-center items-center">
@@ -34,6 +34,7 @@ const Home = ({ props }: ServerSideIndexProps) => {
           Powered by @ta93abe
         </a>
       </footer>
+    </div>
     </div>
   )
 }
